ab122812334a51ab7d3fa930eaf26a16fa58da8e
Manzanit0 <unico.javier@gmail.com>
2019-08-19 12:54:09 +0200

    Add async multiple url fetching

--- lib/spidey.ex
+++ lib/spidey.ex
@@ -1,6 +1,16 @@
 defmodule Spidey do
   @content Application.get_env(:spidey, :content)
 
+  def crawl(urls) when is_list(urls) do
+    urls
+    |> Enum.map(&crawl_async/1)
+    |> Enum.map(&Task.await/1)
+  end
+
+  def crawl_async(url) do
+    Task.async(fn ->crawl(url) end)
+  end
+
   def crawl(url) do
     url
     |> @content.get!()
