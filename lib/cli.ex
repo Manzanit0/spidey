e80e14746294adebacc5650697a9086fdb8e898a
Manzanit0 <unico.javier@gmail.com>
2019-08-20 19:52:32 +0200

    Add options to CLI app

--- lib/cli.ex
+++ lib/cli.ex
@@ -3,11 +3,28 @@ defmodule CLI do
 
   def main(args) do
     args
-    |> OptionParser.parse(strict: [site: :string])
+    |> parse_params()
+    |> execute()
+  end
+
+  defp parse_params(args) do
+    args
+    |> OptionParser.parse(strict: [site: :string, help: :boolean])
     |> elem(0)
-    |> Keyword.get(:site)
+  end
+
+  def execute(help: true) do
+    IO.puts("The two available parameters are --help and --site")
+  end
+
+  def execute(site: site) do
+    site
     |> Spidey.new()
     |> Spidey.crawl()
     |> Enum.map(&IO.inspect/1)
+  end
+
+  def execute(_) do
+    IO.puts("That is not a valid spidey command. See 'spidey --help'.")
   end
 end
