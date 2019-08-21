e5edcba5f367f0c8f93a0c03a9dd1b8fc578342d
Manzanit0 <unico.javier@gmail.com>
2019-08-21 12:07:49 +0200

    Add tests for File module

---
+++ test/file_test.exs
@@ -0,0 +1,20 @@
+defmodule FileTest do
+  use ExUnit.Case, async: true
+
+  @test_file "test_file.txt"
+
+  setup do
+    on_exit fn ->
+      File.rm!(@test_file)
+    end
+  end
+
+  test "save a list of urls in sitemap format" do
+    urls = ["one.com", "two.co.uk", "three.es"]
+
+    :ok = Core.File.save(urls, @test_file)
+
+    assert File.exists?(@test_file)
+    assert {:ok, "one.com\ntwo.co.uk\nthree.es"} == File.read(@test_file)
+  end
+end
