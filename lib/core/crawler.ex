e4860f613a558ac8f10ce001bb4756d6b17cd777
Manzanit0 <unico.javier@gmail.com>
2020-08-11 08:35:31 +0100

    Add timer to crawl function so we can optimise on the fly

--- lib/core/crawler.ex
+++ lib/core/crawler.ex
@@ -18,8 +18,14 @@ defmodule Spidey.Core.Crawler do
   def crawl(%CrawlResult{pending: []}), do: UrlStore.retrieve_all()
 
   def crawl(%CrawlResult{} = cr) do
-    cr.pending
-    |> scan_async()
+    t0 = DateTime.utc_now()
+
+    urls = cr.pending |> scan_async()
+
+    t1 = DateTime.utc_now()
+    IO.puts("Fetching content took #{diff(t0, t1)} ms")
+
+    urls
     |> Filters.process_relative_urls(cr.seed)
     |> Filters.strip_query_params()
     |> Filters.strip_trailing_slashes()
@@ -30,7 +36,15 @@ defmodule Spidey.Core.Crawler do
     |> Enum.uniq()
     |> Enum.map(&push_to_stores/1)
 
-    crawl(%CrawlResult{cr | pending: Queue.take(50)})
+    t2 = DateTime.utc_now()
+    IO.puts("Processing URLs took #{diff(t1, t2)} ms")
+
+    pending = Queue.take(50)
+
+    t3 = DateTime.utc_now()
+    IO.puts("Dequeueing 50 URLs took #{diff(t2, t3)} ms\n")
+
+    crawl(%CrawlResult{cr | pending: pending})
   end
 
   def scan(url) when is_binary(url) do
@@ -63,5 +77,27 @@ defmodule Spidey.Core.Crawler do
   defp push_to_stores(url) do
     Queue.push(url)
     UrlStore.add(url)
+  end
+
+  @doc """
+  Simple diffing function to time short snippets of code using
+  DateTime.utc_now/0.
+
+  Take into account that it's just to get ballpark numbers. The approach is
+  inherently broken.
+
+  ## Example
+
+  iex> t0 = DateTime.utc_now()
+  iex> t1 = DateTime.utc_now()
+  iex> diff(t0, t1)
+  0.02
+  """
+  def diff(%DateTime{} = t0, %DateTime{} = t1) do
+    minutes = t1.minute - t0.minute
+    seconds = t1.second + minutes * 60 - t0.second
+    microseconds = elem(t1.microsecond, 0) + seconds * 1_000_000 - elem(t0.microsecond, 0)
+
+    microseconds / 1_000
   end
 end
