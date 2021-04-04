6a1420828037f730c5a4c432015a155a522e89a0
Manzanit0 <unico.javier@gmail.com>
2021-04-04 12:06:23 +0200

    Guard ETS access against inexisting table

--- lib/spidey/crawler/url_store.ex
+++ lib/spidey/crawler/url_store.ex
@@ -1,37 +1,58 @@
 defmodule Spidey.Crawler.UrlStore do
-  def init!(seed, pool_name) do
-    case :ets.info(table_name(pool_name)) do
-      :undefined ->
-        :ets.new(table_name(pool_name), [:set, :public, :named_table])
+  require Logger
 
-      _ ->
-        raise "ETS table with name #{table_name(pool_name)} already exists."
+  def init!(seed, pool_name) do
+    if table_exists?(pool_name) do
+      raise "ETS table with name #{table_name(pool_name)} already exists."
     end
 
+    :ets.new(table_name(pool_name), [:set, :public, :named_table])
     add(seed, pool_name)
   end
 
   def teardown(pool_name) do
-    :ets.delete(table_name(pool_name))
+    with true <- table_exists?(pool_name) do
+      :ets.delete(table_name(pool_name))
+    else
+      _ ->
+        {:error, :undefined_table}
+    end
   end
 
   def add(url, pool_name) do
-    :ets.insert_new(table_name(pool_name), {url})
+    with true <- table_exists?(pool_name) do
+      :ets.insert_new(table_name(pool_name), {url})
+    else
+      _ ->
+        {:error, :undefined_table}
+    end
   end
 
   def exists?(url, pool_name) do
-    case :ets.lookup(table_name(pool_name), url) do
-      [] -> false
-      _ -> true
+    with true <- table_exists?(pool_name) do
+      case :ets.lookup(table_name(pool_name), url) do
+        [] -> false
+        _ -> true
+      end
+    else
+      _ ->
+        {:error, :undefined_table}
     end
   end
 
   def retrieve_all(pool_name) do
-    pool_name
-    |> table_name()
-    |> :ets.match({:"$1"})
-    |> List.flatten()
+    with true <- table_exists?(pool_name) do
+      pool_name
+      |> table_name()
+      |> :ets.match({:"$1"})
+      |> List.flatten()
+    else
+      _ ->
+        {:error, :undefined_table}
+    end
   end
 
   defp table_name(pool_name), do: :"#{pool_name}_urls"
+
+  defp table_exists?(pool_name), do: :undefined != :ets.info(table_name(pool_name))
 end
